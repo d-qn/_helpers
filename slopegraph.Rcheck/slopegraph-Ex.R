f1e0fbb8d8038ab9b0b9ca1bf56fb59793340fba
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2014-11-25 09:27:46 +0100

    slopegraph collapse label if no multiple colors
    clean help page

--- slopegraph.Rcheck/slopegraph-Ex.R
+++ slopegraph.Rcheck/slopegraph-Ex.R
@@ -11,7 +11,7 @@ nameEx("slopegraph")
 flush(stderr()); flush(stdout())
 
 ### Name: slopegraph
-### Title: Slopegraph chart
+### Title: Slopegraph charts
 ### Aliases: slopegraph
 
 ### ** Examples
@@ -24,6 +24,27 @@ slopegraph(test, rescaleByColumn = T)
 test <- data.frame(x = 1:10, y = c(-5:-1, c(11,11,11, 15,15)), z = c(2,2,2, 4:7, 9,9,9), row.names = letters[1:10])
 test.col <- rep(c("green", "red", "blue", "green", "blue"), 2)
 slopegraph(test, rescaleByColumn = F, col.line=test.col, col.lab=test.col, , cex.lab = 0.6, cex.num = 0.6, offset.x = 0.05, lab.sep = 0.2)
+
+
+
+cleanEx()
+nameEx("slopegraph2")
+### * slopegraph2
+
+flush(stderr()); flush(stdout())
+
+### Name: slopegraph2
+### Title: Slopegraph charts
+### Aliases: slopegraph2
+
+### ** Examples
+
+pctgdp <- data.frame(country = factor(c("Sweden", "Netherlands", "Norway", "Britain", "France", "Germany", "Belgium",
+  "Canada", "Finland", "Italy","United States","Greece", "Switzerland", "Spain", "Japan")),
+	pct1970 = c(46.9,44,43.5,40.7,39,37.5,35.2,35.2,34.9,30.4,30.3,26.8,26.5,22.5,20.7),
+	pct1979 = c(57.4,55.8,52.2,39,43,43,43,35.8,38.2,35.7,32.5,30.6,33.2,27.1,26.6)
+)
+slopegraph2(pctgdp$pct1970, pctgdp$pct1979, pctgdp$country)
 
 
 
--- slopegraph/R/slopegraph.R
+++ slopegraph/R/slopegraph.R
@@ -1,23 +1,24 @@
 #' Slopegraph charts
 #'
-#' Plot a minimalistic Tuffte style slopegraph chart
+#' Plot a minimalistic Tuffte style slopegraph chart. WARNING if 'col.lab' is not of length 1, colliding x labels are not
+#' collapsed with \code{\link{paste}}}, but equally spaced!
 #'
-#' @param df a data.frame that will be plotted as slopegraph
-#' @param rescaleByColumn, a logical of length 1, shall each series of observations be normalized?
-#' @param xlim, ylim vectors of length 2 which sets chart area based on the data unit
-#' @param main a character vector used as the plot title
-#' @param yaxt, xaxt, xlab, ylab, passed on to \code{\link{par}}
-#' @param labels a character string, the labels to be displayed for each slope lines
-#' @param labpos.left, labpos.right a numeric of length 1, the position on the x-axis for the labels
+#' @param df A data.frame that will be plotted as slopegraph.
+#' @param rescaleByColumn  A logical of length 1, shall each series of observations be normalized?
+#' @param xlim,ylim  Vectors of length 2 which sets chart area based on the data unit.
+#' @param main A character vector used as the plot title.
+#' @param yaxt,xaxt,xlab,ylab Passed on to \code{\link{par}}.
+#' @param labels A character string, the labels to be displayed for each slope lines
+#' @param labpos.left,labpos.right A numeric of length 1, the position on the x-axis for the labels
 #' @param collapse.label a character, for colliding labels at the same y the spacer to use
-#' @param lab.sep a numeric of length 1, for colliding labels the space in the x-axis unit
-#' @param col.lines, col.lab vectors of length 1 or of length df, specifying the line and label colors
-#' @param col.num, col.xaxt vectors of length 1 specifying the numerical value and x-axis colors
-#' @param offset.x, offset.lab vectors of length 1 specifying in the x-axis unit the line to numerical value and labels offsets.
-#' @param cex.lab, cex.num  vectors of length 1 specifying the cex of axis and nuermical values, see \code{cex.axis} and \code{cex} in \code{\link{par}}
+#' @param lab.sep a numeric of length 1, only used if 'col.lab' is not of length 1 for the spacing of multiple labels on the same row (in the x-axis unit).
+#' @param col.lines,col.lab vectors of length 1 or of length df, specifying the line and label colors
+#' @param col.num,col.xaxt vectors of length 1 specifying the numerical value and x-axis colors
+#' @param offset.x,offset.lab vectors of length 1 specifying in the x-axis unit the line to numerical value and labels offsets.
+#' @param cex.lab,cex.num  vectors of length 1 specifying the cex of axis and nuermical values, see \code{cex.axis} and \code{cex} in \code{\link{par}}
 #' @param font.lab, a vector of length 1 specifying the font weight, as defined in \code{font} in \code{\link{par}}
-#' @param lty, lwd vectors of length 1 or of length df specifiyng the line type and width, see \code{\link{par}}
-#' @param mai, plot margin as defined in \code{\link{par}}
+#' @param lty,lwd vectors of length 1 or of length df specifiyng the line type and width, see \code{\link{par}}
+#' @param mai plot margin as defined in \code{\link{par}}
 #' @importFrom scales rescale
 #' @references https://gist.github.com/leeper/7158678
 #' @export
@@ -30,13 +31,13 @@
 #' test <- data.frame(x = 1:10, y = c(-5:-1, c(11,11,11, 15,15)), z = c(2,2,2, 4:7, 9,9,9), row.names = letters[1:10])
 #' test.col <- rep(c("green", "red", "blue", "green", "blue"), 2)
 #' slopegraph(test, rescaleByColumn = F, col.line=test.col, col.lab=test.col, , cex.lab = 0.6, cex.num = 0.6, offset.x = 0.05, lab.sep = 0.2)
-#'
+#' slopegraph(test, rescaleByColumn = F, col.line=test.col, cex.lab = 0.6, cex.num = 0.6, offset.x = 0.05, lab.sep = 0.2, xaxt = 's')
 
 slopegraph <- function(
   df,
   rescaleByColumn = T,
-  xlim = c(0.5,ncol(df)+0.5),
-  ylim = c(min(df, na.rm = T)-diff(range(df, na.rm = T))/100,max(df, na.rm = T)+diff(range(df, na.rm = T))/100),
+  xlim = c(0,ncol(df) + 1),
+  ylim = c(min(df, na.rm = T) - diff(range(df, na.rm = T))/100,max(df, na.rm = T)+diff(range(df, na.rm = T))/100),
   main = NULL,
   yaxt = 'n',
   xaxt = 'n',
@@ -92,30 +93,61 @@ slopegraph <- function(
   }
   rownames(df.rescale) <- rownames(df)
 
+  ## left-side labels
   l <- df.rescale[,1] # I MAY WANT TO BIN THESE SO THAT CLOSE VALUES DON'T OVERLAP
-  leftlabs <- lapply(split(rownames(df.rescale),l), paste, collapse = collapse.label)
 
-  # print text for no labels on the same row
-  text(1 - offset.lab, as.numeric(names(leftlabs)),
+  if(length(unique(col.lab)) == 1) {
+	 leftlabs <- lapply(split(rownames(df.rescale),l), paste, collapse = collapse.label)
+ 	 text(1 - offset.lab, as.numeric(names(leftlabs)),
        col=col.lab[match(as.numeric(names(leftlabs)), l)], leftlabs, pos=labpos.left, cex=cex.lab, font=font.lab)
 
-  # right-side labels
-  r <- df.rescale[,ncol(df)] # I MAY WANT TO BIN THESE SO THAT CLOSE VALUES DON'T OVERLAP
-  rightlabs <- lapply(split(rownames(df.rescale),r), paste, collapse=collapse.label)
+  } else {
+	  leftlabs <- lapply(split(rownames(df.rescale),l), paste, collapse.label, sep="")
+	  lab.dup <- sapply(leftlabs, length) > 1
+	  # print text for single labels on the same row
+	  text(1 - offset.lab, as.numeric(names(leftlabs)[!lab.dup]),
+	       col=col.lab[match(as.numeric(names(leftlabs)[!lab.dup]), l)], leftlabs[!lab.dup], pos=labpos.left, cex=cex.lab, font=font.lab)
+	  # print multiple labels on the same row
+	  sapply(as.numeric(names(lab.dup)[lab.dup]), function(pos) {
+	    idx <- l == pos
+	    text(c(1 - offset.lab, 1:(sum(idx)-1) * -lab.sep + (1 - offset.lab)),
+	         pos, col=col.lab[idx], unlist(leftlabs[as.character(pos)]), pos=labpos.left,
+	         cex=cex.lab, font=font.lab)
+	  })
+  }
 
-  # print text for no labels on the same row
-  text(ncol(df)+offset.lab, as.numeric(names(rightlabs)),
-       col=col.lab[match(as.numeric(names(rightlabs)), r)], rightlabs, pos=labpos.right, cex=cex.lab, font=font.lab)
+  ## right-side labels
+  r <- df.rescale[,ncol(df)] # I MAY WANT TO BIN THESE SO THAT CLOSE VALUES DON'T OVERLAP
+  if(length(unique(col.lab)) == 1) {
+    rightlabs <- lapply(split(rownames(df.rescale),r), paste, collapse=collapse.label)
+    text(ncol(df)+offset.lab, as.numeric(names(rightlabs)),
+         col=col.lab[match(as.numeric(names(rightlabs)), r)], rightlabs, pos=labpos.right, cex=cex.lab, font=font.lab)
+  } else {
+	  rightlabs <- lapply(split(rownames(df.rescale),r), paste, collapse.label, sep="")
+	  lab.dup <- sapply(rightlabs, length) > 1
+
+	  # print text for single labels on a row
+	  text(ncol(df)+offset.lab, as.numeric(names(rightlabs)[!lab.dup]),
+	       col=col.lab[match(as.numeric(names(rightlabs)[!lab.dup]), r)], rightlabs[!lab.dup], pos=labpos.right, cex=cex.lab, font=font.lab)
+	  # print multiple labels on the same row
+	  sapply(as.numeric(names(lab.dup)[lab.dup]), function(pos) {
+	    idx <- r == pos
+	    text(c(ncol(df) + offset.lab, 1:(sum(idx)-1) * lab.sep + (ncol(df) + offset.lab)),
+	         pos, col=col.lab[idx], unlist(rightlabs[as.character(pos)]), pos=labpos.right,
+	         cex=cex.lab, font=font.lab)
+	  })
+  }
+
 
   # numeric value labels
   # deal with duplicate value labels (i.e., not double printing anything)
   df2 <- do.call(cbind,lapply(df, function(y) {y[duplicated(y)] <- ''; y}))
 
   # print them
-  apply(cbind(df.rescale,df2),1, function(y)
+  apply(cbind(df.rescale,df2),1, function(y) {
     text(1:ncol(df), as.numeric(y[1:ncol(df)]), y[(ncol(df) + 1):(2*ncol(df))],
          col = col.num, cex = cex.num, font = font.num)
-  )
+  })
   # draw lines
   for(i in 1:nrow(df.rescale)){
     mapply(function(x1,y1,x2,y2,...){
