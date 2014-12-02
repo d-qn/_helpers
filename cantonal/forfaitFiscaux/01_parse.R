59c312a4f60b85d2bef4cb83e20c8e8ed517d037
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2014-12-02 11:20:36 +0100

    update

--- cantonal/forfaitFiscaux/01_parse.R
+++ cantonal/forfaitFiscaux/01_parse.R
@@ -14,7 +14,7 @@ iorder <- match(canton_names$iso2, rownames(readFile))
 data <- readFile[iorder,]
 
 newCol <- 'Part en % des forfaits fiscaux dans les recettes fiscales cantonales'
-data[,newCol] <- data[,"impôt.cantonal"] / data[,'Recettes.fiscales.cantonales']
+data[,newCol] <- (data[,"impôt.cantonal"] / data[,'Recettes.fiscales.cantonales']) * 100
 
 coln.sub <- c("Nombre.de.forfaits", "impôt.cantonal", "totale.des.recettes.fiscales", newCol)
 
